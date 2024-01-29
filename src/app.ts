421824e33b2c946485b18f2823c433fe9f851792
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2024-01-29 18:50:17 +0100

    feat: prisma database connection

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,14 @@ dotenv.config();
 import http from "http";
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 import bodyParser from 'body-parser';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const host = process.env.HOST || 'localhost';
 const port = Number(process.env.PORT || 8000);
 const httpServer = http.createServer(app);
+
+const prisma = new PrismaClient()
 
 app.use(bodyParser.json());
 
@@ -33,6 +36,27 @@ app.get('/api/v1', (req, res) => {
     });
 });
 
+app.post('/api/v1/products', async (req, res) => {
+    const { name, description, price, category, quantity } = req.body;
+
+    if (!name || !description || !price || !category || !quantity) {
+        return res.status(400).json({ error: 'Name, price, category, and quantity are required fields' });
+    }
+
+    const newProduct = await prisma.product.create({
+        data: {
+          name, 
+          description,
+          price,
+          category,
+          quantity,
+        },
+    });
+
+    res.status(201).json(newProduct);
+
+});
+
 app.all('*', (req, res) => {
     return res.status(StatusCodes.NOT_FOUND).json({
         success: false,
@@ -42,14 +66,23 @@ app.all('*', (req, res) => {
 
 const startServer = async () => {
     try {
-        httpServer.listen(port, host, () => {
-            console.log(`🌟 🛠️  [SERVER] - Server is listening on http://${host}:${port}`);
-        });
-    } catch(error){
-        console.log(`[SERVER] - Failed to start. Encountered an error during startup.`, error);
-    }   
+        await prisma.$connect();
+        console.log(`[DATABASE] - Database connection has been successfully established.`);
+
+        try {
+            httpServer.listen(port, host, () => {
+                console.log(`🌟 🛠️  [SERVER] - Server is listening on http://${host}:${port}`);
+            });
+        } catch (error){
+            console.log(`[SERVER] - Failed to start. Encountered an error during startup.`, error);
+        } 
+    } catch (error) {
+        console.log(`[DATABASE] - Server not started due to database connection error.`, error);
+    }
+  
 };
 
 startServer();
 
 export default app;
+ 
