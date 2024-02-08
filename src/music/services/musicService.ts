e5413e14cb50dab834d65d8e22d197ed07db5892
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2024-02-08 12:50:36 +0100

    feat: add song validation

--- src/music/services/musicService.ts
+++ src/music/services/musicService.ts
@@ -1,6 +1,10 @@
 import UserRepository from '../../user/repositories/userRepository';
 import MusicRepository from '../repositories.ts/musicRepository';
 import BadRequest from '../../errors/BadRequest';
+import {
+    USER_NOT_FOUND,
+    LISTENERS_NOT_ALLOWED,
+} from '../utils/constants';
 
 class MusicService {
     private userRepository: UserRepository;
@@ -18,7 +22,13 @@ class MusicService {
         const user = await this.userRepository.getUserById(userId);
 
         if(!user) {
-            throw new BadRequest('USER_NOT_FOUND');
+            throw new BadRequest(USER_NOT_FOUND);
+        }
+
+        const access = user.access;
+
+        if(access === 'listener') {
+            throw new BadRequest(LISTENERS_NOT_ALLOWED);
         }
 
         const music = await this.musicRepository.addSong(
---
+++ src/music/utils/constants.ts
@@ -0,0 +1,2 @@
+export const USER_NOT_FOUND = "USER_NOT_FOUND";
+export const LISTENERS_NOT_ALLOWED = "Sorry, listeners CANNOT perform this action.";
